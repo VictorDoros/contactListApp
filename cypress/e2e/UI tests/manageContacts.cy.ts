bddd0ecb06bce633d0416cdf2dc96e1fb44b53a0
Victor Doros <dorosvictor@gmail.com>
2024-08-30 17:34:37 +0300

    install cypress map and try test for add contact

--- cypress/e2e/UI tests/manageContacts.cy.ts
+++ cypress/e2e/UI tests/manageContacts.cy.ts
@@ -2,27 +2,54 @@ import LogInPage from "../../pages/loginPage"
 import User from "../../models/user"
 import AddContactPage from "../../pages/addContactPage"
 import "cypress-plugin-api"
+import RegisterUser from "../../pages/registerUser"
+import 'cypress-map'
 
 describe("Add contact", () => {
   let logInPage: LogInPage
   let user: User
   let addContactPage: AddContactPage
+  let registerUser: RegisterUser
 
   beforeEach(() => {
     logInPage = new LogInPage()
     user = new User()
     addContactPage = new AddContactPage()
+    registerUser = new RegisterUser()
+
+    registerUser.loadRegisterPage()
+    registerUser.checkRegisterPage()
+    registerUser.registerUser(user)
   })
 
-  it("Should be able to add a valid contact", () => {
-    logInPage.loadLoginPage()
-    logInPage.logIn()
-    logInPage.submitLogIn()
-    logInPage.checkUserLoggedIn
-
+  it.skip("Should be able to add a valid contact", () => {
     addContactPage.loadAddContact()
     addContactPage.addContact(user)
 
     addContactPage.checkError().should("not.exist")
   })
-})
+
+  it("Should be able to add a valid contact", () => {
+    // cy.get("tbody td").then((tbody) => {
+    //   expect(tbody.find.length).to.eq(0)
+    // })
+    addContactPage.loadAddContact()
+    addContactPage.addContact(user)
+
+    addContactPage.checkError().should("not.exist")
+    cy.get("table tr").then((tbody) => {
+      expect(tbody.find.length).to.eq(1)
+    })
+    
+    let array = []
+    cy.get("table tr td").then((rows) => {
+      expect(rows.length).to.eq(8)
+    })
+
+    cy.get('table tr').find("td").not("[hidden='true']").map('innerText').print()
+
+
+  })
+    
+  })
+
