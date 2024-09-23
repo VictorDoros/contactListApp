5cea40b4bf5ad811bb2d0f9dc9c3dcd463c5ea28
Victor Doros <dorosvictor@gmail.com>
2024-09-23 19:39:18 +0300

    remove unnecessary lines of code from API tests

--- cypress/e2e/API tests/addContactsUsingAPI.cy.ts
+++ cypress/e2e/API tests/addContactsUsingAPI.cy.ts
@@ -1,21 +1,15 @@
 import User from "../../models/user"
 import Environment from "../../models/environment"
-import RegisterUser from "../../pages/registerUser"
-import AddContactPage from "../../pages/addContactPage"
 import APIRequests from "../../pages/apiRequests"
 
 describe("Add contact - API", { tags: ["@api", "@addContactsAPI"] }, () => {
   let user: User
   let env: Environment
-  let registerUser: RegisterUser
-  let addContactPage: AddContactPage
   let apiRequest: APIRequests
 
   beforeEach(() => {
     user = new User()
     env = new Environment()
-    registerUser = new RegisterUser()
-    addContactPage = new AddContactPage()
     apiRequest = new APIRequests()
 
     apiRequest.registerUsingAPI(user, env)
